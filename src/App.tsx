10ad27bdb488c4675afc7c95ac0c512e76685848
Odonno <bottiau.david@laposte.net>
2019-03-24 23:23:55 +0100

    feat: use react hooks and rxjs-spy tag to spy observables

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { useObservable } from "rxjs-hooks";
 import { interval } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
+import { tag } from "rxjs-spy/operators/tag";
+import { create } from "rxjs-spy";
+
+const spy = create();
+
+const now$ = interval(1000).pipe(
+  map(_ => new Date()),
+  startWith(new Date()),
+  tag("now")
+);
 
 export const App = () => {
-  const now = useObservable<Date>(
-    _ => interval(1000).pipe(
-      map(_ => new Date())
-    ),
-      new Date()
-  );
-
+  const now = useObservable<Date>(_ => now$);
+  
   return (
     <div>
-      {now.toLocaleString()}
+      {now && now.toLocaleString()}
     </div>
   );
 }
 
 export default App;
+
+spy.log("now");
